0363ca899c0b9cd9b5e9dfb5c1d212d422becbfb
Silas <silas23.fr@gmail.com>
2022-01-26 22:36:13 -0300

    feat: connect to mongodb

--- src/server.js
+++ src/server.js
@@ -1,17 +1,48 @@
 import express from "express";
-import cors from "cors";
+import { MongoClient, ObjectId } from "mongodb";
 import { json } from "express/lib/response";
-import { MongoCliente } from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors";
+import dayjs from "dayjs";
+dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(json());
-
-server.post("/participants", (req, res) => {
-  res.send("OK");
+const mongoCliente = new MongoClient("mongodb://localhost:27017");
+let db;
+mongoCliente.connect(() => {
+  db = mongoCliente.db("batepapo_uol");
 });
 
-server.get("/participants", (req, res) => {});
+server.post("/participants", async (req, res) => {
+  try {
+    await db
+      .collection("participants")
+      .insertOne({ name: req.body, lastStatus: Date.now() });
+    await db.collection("messages").insertOne({
+      from: participant.name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs(participant.lastStatus).format("HH:mm:ss"),
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+server.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participants").find().toArray();
+    res.send(participants);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 server.post("/messages", (req, res) => {});
 
