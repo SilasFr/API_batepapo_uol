173b8f228f989f952c7c31fbf1cca9fb9daa5b03
Silas <silas23.fr@gmail.com>
2022-01-29 11:52:13 -0300

    feat: validating user login

--- src/server.js
+++ src/server.js
@@ -3,20 +3,40 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
 import cors from "cors";
+import Joi from "joi";
 dotenv.config();
 
 const mongoCliente = new MongoClient(process.env.MONGO_URI);
 let db;
+mongoCliente.connect(() => {
+  db = mongoCliente.db("batepapo_uol");
+});
 
 const server = express();
 server.use(cors());
 server.use(json());
 
+const participantSchema = Joi.object().keys({
+  name: Joi.string().required().min(3).max(30),
+});
+
 server.post("/participants", async (req, res) => {
-  mongoCliente.connect(() => {
-    db = mongoCliente.db("batepapo_uol");
-  });
+  const validation = Joi.validate(req.body, participantSchema);
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  }
   const participant = { ...req.body, lasStatus: Date.now() };
+
+  const allParticipants = await db.collection("participants").find().toArray();
+  for (let user of allParticipants) {
+    console.log(user);
+    if (user.name === participant.name) {
+      res.sendStatus(409);
+      return;
+    }
+  }
+
   try {
     await db.collection("participants").insertOne(participant);
     await db.collection("messages").insertOne({
@@ -27,33 +47,21 @@ server.post("/participants", async (req, res) => {
       time: dayjs(participant.lastStatus).format("HH:mm:ss"),
     });
     res.sendStatus(201);
-    mongoCliente.close();
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
-    mongoCliente.close();
   }
 });
 
 server.get("/participants", async (req, res) => {
-  mongoCliente.connect(() => {
-    db = mongoCliente.db("batepapo_uol");
-  });
   try {
     const participants = await db.collection("participants").find().toArray();
     res.send(participants);
-    mongoCliente.close();
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
-    mongoCliente.close();
   }
 });
 
 server.post("/messages", async (req, res) => {
-  mongoCliente.connect(() => {
-    db = mongoCliente.db("batepapo_uol");
-  });
   try {
     const message = {
       ...req.body,
@@ -62,66 +70,61 @@ server.post("/messages", async (req, res) => {
     };
     await db.collection("messages").insertOne(message);
     res.sendStatus(201);
-    mongoCliente.close();
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
-    mongoCliente.close();
   }
 });
 
 server.get("/messages", async (req, res) => {
-  mongoCliente.connect(() => {
-    db = mongoCliente.db("batepapo_uol");
-  });
   try {
     const user = req.headers.user;
     const limit = parseInt(req.query.limit);
-    const messages = messagesUserCanSee(user);
-
+    const messages = db
+      .collection("messages")
+      .find({ $or: [{ to: "todos" }, { to: user }] });
     if (limit) {
       res.send(messages.slice(-limit));
     } else {
       res.send(messages);
     }
-    mongoCliente.close();
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
-    mongoCliente.close();
   }
 });
 
 server.post("/status", (req, res) => {
-  mongoCliente.connect(() => {
-    db = mongoCliente.db("batepapo_uol");
-  });
   try {
-    mongoCliente.close();
   } catch (error) {
-    console.log(error);
     res.send(500);
-    mongoCliente.close();
   }
 });
 
-async function messagesUserCanSee(user) {
-  mongoCliente.connect(() => {
-    db = mongoCliente.db("batepapo_uol");
-  });
-  const seeableMessages = [];
-  const messages = await db.collection("messages").find().toArray();
-
-  if (!user) {
-    return messages;
+setInterval(async () => {
+  try {
+    const limit = Date.now() - 10000;
+    const result = await db
+      .collection("participants")
+      .deleteMany({ lastStatus: { $gt: limit } });
+    console.log(result);
+  } catch (e) {
+    console.log(e);
   }
+}, 15000);
 
-  for (let message of messages) {
-    if (message.to === "Todos" || message.to === user) {
-      seeableMessages.push(message);
-    }
-  }
-  return seeableMessages;
-}
+// async function messagesUserCanSee(user) {
+//   const seeableMessages = [];
+//   const messages = await db.collection("messages").find().toArray();
+
+//   if (!user) {
+//     return messages;
+//   }
+
+//   for (let message of messages) {
+//     if (message.to === "Todos" || message.to === user) {
+//       seeableMessages.push(message);
+//     }
+//   }
+//   return seeableMessages;
+// }
 
 server.listen(4000);
